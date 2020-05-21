1f7ebceaf2cf46e481269aff5c72e445333b3c42
Jaycee-Speedbug <speedbug@hotmail.fr>
2020-05-21 15:48:23 +0200

    Initial project set. One command line tool and one class project.

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using AeroCalcCore;
+
+namespace CLtool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AeroCalcCommandProcessor CommandProcessor = new AeroCalcCommandProcessor();
+            AeroCalcCommand Command;
+            bool run = true;
+
+            //
+            // Initialisation & Accueil
+            //
+            Command = CommandProcessor.process(AeroCalcCommand.CMD_WORD_INIT_INTERPRETER);
+            if (Command.eventCode == AeroCalcCommand.EVENTCODE_INIT_SUCCESSFULL) {
+                // Initialisation réussie
+                Console.WriteLine(Command.txtResult);
+            }
+            else {
+                // Echec de l'initialisation
+                Console.WriteLine("Exiting AirCalc...");
+                DateTime instant = DateTime.Now;
+                instant = instant.AddSeconds(2.5);
+                while (DateTime.Now.Second < instant.Second) { }
+                run = false;
+            }
+
+            // Boucle principale de l'interpréteur de commandes en ligne
+            while (run) {
+
+                // Prompt
+                Console.Write("<>");
+
+                // Lecture de la console et transfert vers le processeur de commande
+                Command = CommandProcessor.process(Console.ReadLine());
+
+                //
+                // Traitement particulier de la commande EXIT
+                //
+                if (Command.action == AeroCalcCommand.ACTION_EXIT)
+                {
+                    Console.WriteLine("Exiting AirCalc...");
+                    DateTime instant = DateTime.Now;
+                    instant = instant.AddSeconds(2.5);
+                    while (DateTime.Now.Second < instant.Second) { }
+                    //run = false;
+                    break;
+                }
+
+                //
+                // Affichage du résultat des commandes
+                //
+                Console.WriteLine(Command.txtResult);
+                if (Command.eventCode <= AeroCalcCommand.EVENTCODE_INIT_VALUE)
+                {
+                    // Echec du traitement de la commande, ajout du commentaire en seconde ligne
+                    Console.WriteLine(Command.txtComment);
+                }
+
+            }
+
+        } // Main()
+    
+
+
+        private void exitInterpreter() {
+        }
+
+        }
+    }
+}
