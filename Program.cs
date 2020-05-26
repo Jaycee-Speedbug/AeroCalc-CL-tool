b6b23fac9ffdbe0e6d2d093eda6b1903c134c839
Jaycee-Speedbug <speedbug@hotmail.fr>
2020-05-26 11:03:07 +0200

    Added: Command Line option for config file

--- Program.cs
+++ Program.cs
@@ -14,10 +14,22 @@ namespace CLtool
             AeroCalcCommandProcessor CommandProcessor = new AeroCalcCommandProcessor();
             AeroCalcCommand Command;
             bool run = true;
-            string configurationFile = "config" + Path.DirectorySeparatorChar + "_config.xml";
+            string cmdLineOptions = " ";
+
+            // TODO: Provisoire, améliorer la prise en charge des options de ligne de commande
+            if  (args.Length == 0) {
+                // Default configuration file path
+                cmdLineOptions = "config" + Path.DirectorySeparatorChar + "config.xml";
+            }
+            else {
+                foreach (string str in args)
+                {
+                    cmdLineOptions += str + " ";
+                }
+            }
 
             // Initialisation & Accueil
-            Command = CommandProcessor.process(AeroCalcCommand.CMD_WORD_INIT_INTERPRETER + " " + configurationFile);
+            Command = CommandProcessor.process(AeroCalcCommand.CMD_WORD_INIT_INTERPRETER + cmdLineOptions);
             if (Command.eventCode == AeroCalcCommand.EVENTCODE_INIT_SUCCESSFULL) {
                 // Initialisation réussie
                 Console.WriteLine(Command.txtResult);
@@ -35,7 +47,7 @@ namespace CLtool
             while (run) {
 
                 // Prompt
-                Console.Write("<>");
+                Console.Write("--o-Ô-o--");
 
                 // Lecture de la console et transfert vers le processeur de commande
                 Command = CommandProcessor.process(Console.ReadLine());
